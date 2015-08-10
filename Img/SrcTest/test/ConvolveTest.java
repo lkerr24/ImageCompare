cd86138f71d91d52f62208a003326be09efd3184
lkerr24 <lkerr24@qub.ac.uk>
2015-08-10 23:44:01 +0100

    More tests

--- Img/SrcTest/test/ConvolveTest.java
+++ Img/SrcTest/test/ConvolveTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import static org.junit.Assert.*;
+import imageCompare.Convolve;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -18,7 +19,15 @@ public class ConvolveTest {
 
 	@Test
 	public void testCompare() {
-		fail("Not yet implemented");
+		Convolve cv = new Convolve();
+		int[][] array1 = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15}};
+		int[][] array2 = {{15,14,13,12,11}, {10,9,8,7,6} ,{5,4,3,2,1}};
+		
+		int[][] actual = cv.compare(array1, array2);
+		int[][] expected = {{14,12,10,8,6},{4,2,0,-2,-4}, {-6,-8,-10,-12,-14}};
+		
+		assertArrayEquals(actual, expected);
+		
 	}
 
 }
--- Img/SrcTest/test/GrayscaleTest.java
+++ Img/SrcTest/test/GrayscaleTest.java
@@ -36,8 +36,40 @@ public class GrayscaleTest {
 	}
 
 	@Test
-	public void makeGreyTest() {
-		fail("Not yet implemented");
+	public void makeGreyTest() throws IOException, ImageDecoderException {
+		BufferedImage greyTest = gs.makeGrey("IMG_0062.jpg");
+		int[][] greyLevelsActual = gs.getGreyLevels(greyTest);
+		int[][] greyLevelsExpected;
+		WritableRaster raster = greyTest.getRaster();
+		// for each pixel, if it is not a grey value, the test fails
+		int count=0;
+		for (int i = 0; i < greyTest.getWidth(); i++){
+			for(int j = 0; j < greyTest.getHeight(); j++){
+				if (greyLevels[i][j]>=0 && greyLevels[i][j]<=255){
+					count++;
+				}
+			}
+		}
+		if (count!=(greyLevels.length*greyLevels[0].length)){
+			fail("Not all pixels greyscale");
+		}
+	}
+	@Test
+	public void makeGreyTestInvalidImage() throws IOException, ImageDecoderException {
+		ImageDecoder input = ImageFile.createImageDecoder("IMG_0062.jpg");
+		BufferedImage greyTest = input.decodeAsBufferedImage();
+		int[][] greyLevels = gs.getGreyLevels(greyTest);
+		int count=0;
+		for (int i = 0; i < greyTest.getWidth(); i++){
+			for(int j = 0; j < greyTest.getHeight(); j++){
+				if (greyLevels[i][j]>=0 && greyLevels[i][j]<=255){
+					count++;
+				}
+			}
+		}
+		if (count!=(greyLevels.length*greyLevels[0].length)){
+			fail("Not all pixels greyscale");
+		}
 	}
 	@Test
 	public void getGreyLevelsForBlackImageTest() throws IOException, ImageDecoderException {
@@ -58,7 +90,7 @@ public class GrayscaleTest {
 				expected[i][j] = 0;
 			}
 		}
-		assertSame(actual, expected);
+		assertArrayEquals(actual, expected);
 	}
 	@Test
 	public void getGreyLevelsForWhiteImageTest() throws IOException, ImageDecoderException {
@@ -69,7 +101,7 @@ public class GrayscaleTest {
 		
 		for (int i = 0; i < image.getWidth(); i++) {
 			for (int j = 0; j < image.getHeight(); j++) {
-				int[] colours = {225, 225, 225};
+				int[] colours = {255, 255, 255};
 				blackImage.setPixel(i,j,colours);
 			}
 		}
@@ -86,7 +118,7 @@ public class GrayscaleTest {
 				expected[i][j] = 255;
 			}
 		}
-		assertEquals(actual, expected);
+		assertArrayEquals(actual, expected);
 	}
 	@Test
 	public void createImageTest() {
