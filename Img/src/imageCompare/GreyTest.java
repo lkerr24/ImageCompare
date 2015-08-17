7a648296a7ed3c0fba97fcbc85e1b05c68b49efb
lkerr24 <lkerr24@qub.ac.uk>
2015-08-17 10:57:04 +0100

    Updates to GreyTest to make it easier to demo

--- Img/src/imageCompare/GreyTest.java
+++ Img/src/imageCompare/GreyTest.java
@@ -10,8 +10,7 @@ import javax.swing.JLabel;
 import com.pearsoneduc.ip.op.*;
 import com.pearsoneduc.ip.io.ImageDecoderException;
 /**
- * Trying buffered image
- * This is the class I sent to Paul to show taking in an image, converting it to grayscale and the output
+ * Main method for testing purposes
  * @author laura
  */
 public class GreyTest extends JFrame{
@@ -25,39 +24,64 @@ public class GreyTest extends JFrame{
 		Grayscale gs = new Grayscale();
 		Convolve cv = new Convolve();
 		DivideIntoBlocks dib = new DivideIntoBlocks();
-	
+		
+		// Creating greyscale image for the cosine angle calculation
 		String filename1 = "lenaColour.jpg";
 		BufferedImage greyImage1 = gs.makeGrey(filename1);
+		BufferedImage[][] imgs1 = dib.divide(greyImage1,8,8);
+		
+		// Creating 2D array of grey levels for convolution
 		int[][] greyPixels1 = gs.calculateGreyLevels(greyImage1);
 		int[][] blurPixels1 = cv.convolve(greyPixels1, 1.0f);
 		BufferedImage blurImage1 = gs.createImage(blurPixels1);
-		BufferedImage[][] imgs1 = dib.divide(blurImage1,8,8);
 		
+		// Creating greyscale image for the cosine angle calculation
 		String filename2 = "lenaColour.jpg";
 		BufferedImage greyImage2 = gs.makeGrey(filename2);
+		BufferedImage[][] imgs2 = dib.divide(greyImage2,8,8);
+		
+		// Creating 2D array of grey levels for convolution
 		int[][] greyPixels2 = gs.calculateGreyLevels(greyImage2);
 		int[][] blurPixels2 = cv.convolve(greyPixels2, 2.0f);
 		BufferedImage blurImage2 = gs.createImage(blurPixels2);
-		BufferedImage[][] imgs2 = dib.divide(blurImage2,8,8);
 		
+		//Calculate the average cosine angle for the 2 grey images
 		double aveCosineAngle = dib.aveCosAngle(imgs1, imgs2);
 		System.out.printf("%.10f",aveCosineAngle);
 		
+		// Display the grey image
+		JFrame frameForGrey1 = new JFrame();
+		JLabel viewGrey1 = new JLabel(new ImageIcon(greyImage1));
+		frameForGrey1.add(viewGrey1);
+		frameForGrey1.pack();
+		frameForGrey1.setVisible(true);
+		
+		// display the grey image
+		JFrame frameForGrey2 = new JFrame();
+		JLabel viewGrey2 = new JLabel(new ImageIcon(greyImage2));
+		frameForGrey2.add(viewGrey2);
+		frameForGrey2.pack();
+		frameForGrey2.setVisible(true);
+		
+		// Perform the convolution
 		int[][] blurDiff = cv.compare(blurPixels1, blurPixels2);
 		BufferedImage difference = gs.createImage(blurDiff);
 
+		// Display the blurred image
 		JFrame frameForBlur1 = new JFrame();
 		JLabel view1 = new JLabel(new ImageIcon(blurImage1));
 		frameForBlur1.add(view1);
 		frameForBlur1.pack();
 		frameForBlur1.setVisible(true);
 		
+		// Display the blurred image
 		JFrame frameForBlur2 = new JFrame();
 		JLabel view2 = new JLabel(new ImageIcon(blurImage2));
 		frameForBlur2.add(view2);
 		frameForBlur2.pack();
 		frameForBlur2.setVisible(true);	
 		
+		// Display the difference in the two blurred images
 		JFrame frameForDiff = new JFrame();
 		JLabel view3 = new JLabel(new ImageIcon(difference));
 		frameForDiff.add(view3);
@@ -65,7 +89,7 @@ public class GreyTest extends JFrame{
 		frameForDiff.setVisible(true);	
 		
 		/*
-		//Display the images in a 2D array
+		//DISPLAY THE IMAGES IN A 2D ARRAY
 		int rows = imgs1.length;
 		int cols = imgs1[0].length;
 		JLabel[][] imgsLabel = new JLabel[rows][cols];
@@ -90,6 +114,8 @@ public class GreyTest extends JFrame{
 		frameForMinis.pack();
 		frameForMinis.setVisible(true);
 		*/
+		
+		/* TESTING CONVOLVE METHOD COMPARING WITH EFFORD'S GAUSSIAN BLUR METHOD
 		GaussianKernel gKernel = new GaussianKernel(1.0f);
 		ConvolutionOp co = new ConvolutionOp(gKernel);
 		
@@ -120,5 +146,6 @@ public class GreyTest extends JFrame{
 		frameForExpected.add(view2);
 		frameForExpected.pack();
 		frameForExpected.setVisible(true);
+		*/
 	}
 }
