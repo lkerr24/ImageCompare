51abae11fd3f78c89eab9d8fe646c2e8f24ce148
lkerr24 <lkerr24@qub.ac.uk>
2015-08-20 11:08:59 +0100

    Comparison

---
+++ PageAlert/Comparison.java
@@ -0,0 +1,27 @@
+package qub.ecit.sivs.pagealert;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
+import com.pearsoneduc.ip.io.ImageDecoderException;
+import com.pearsoneduc.ip.op.FFTException;
+import com.pearsoneduc.ip.op.ImageFFT;
+
+public class Comparison{
+	
+	File image1, image2;
+	
+	public Comparison(){
+		image1=null;
+		image2=null;
+	}
+	
+
+
+	
+}
